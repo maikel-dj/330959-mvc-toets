f77ccd5b118311de72c700bf1db9b48501118925
maikel-dj <64279296+maikel-dj@users.noreply.github.com>
2022-03-18 09:27:31 +0100

    Last Commit
    Added sql and finalized project

--- View/country.php
+++ View/country.php
@@ -1,9 +1,17 @@
+<div>
+<h4>ID Land    Hoofdstad   Continent   Aantalinwoners</h>
+</div>
+
 <?php
 
 foreach($countries as $country)
 { ?>
     <div>
+        <?= $country['id'] ?>
         <?= $country['name'] ?>
+        <?= $country['capitalCity'] ?>
+        <?= $country['continent'] ?>
+        <?= $country['population'] ?>
     </div>
 <?php
 }
---
+++ mvcframework.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Gegenereerd op: 18 mrt 2022 om 08:26
+-- Serverversie: 5.7.31
+-- PHP-versie: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mvcframework`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `country`
+--
+
+DROP TABLE IF EXISTS `country`;
+CREATE TABLE IF NOT EXISTS `country` (
+  `id` int(10) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `capitalCity` varchar(300) NOT NULL,
+  `continent` varchar(300) NOT NULL,
+  `population` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `country`
+--
+
+INSERT INTO `country` (`id`, `name`, `capitalCity`, `continent`, `population`) VALUES
+(1, 'Nederland', 'Amsterdam', 'Europa', 17134872),
+(2, 'Belgie', 'Antwerpen', 'Europa', 11560000),
+(3, 'Duitsland', 'Berlijn', 'Europa', 83240000),
+(4, 'Rusland', 'Moskau', 'Europa', 148200000),
+(5, 'Mexico', 'Mexico-Stad', 'Zuid-Amerika', 128900000);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
